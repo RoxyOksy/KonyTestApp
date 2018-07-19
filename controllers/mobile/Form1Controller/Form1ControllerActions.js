e83bee2ee06ef59eae1ed6274ae07cbee9528eed
RoxyOksy <roksana.dp.ua@gmail.com>
2018-07-19 12:34:18 +0300

    create "log in" view

---
+++ controllers/mobile/Form1Controller/Form1ControllerActions.js
@@ -0,0 +1,10 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for loginButton **/
+    AS_Button_j8c155818e2a4e80bb8d24d92376d3d7: function AS_Button_j8c155818e2a4e80bb8d24d92376d3d7(eventobject) {
+        var self = this;
+        return
+    }
+});
